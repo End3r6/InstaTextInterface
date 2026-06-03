bebffd414dfbbd2a82b78ba90589deae2dac4221
JD Swardson <jdsward@sandia.gov>
2026-06-03 09:40:40 -0600

    asasasas

--- song_downloader.py
+++ song_downloader.py
@@ -64,7 +64,7 @@ class SongDownloader(BaseBotApp):
         url = results.all[0].watch_url
         print(url)
 
-        yt = YouTube(url, proxies=proxy, client="WEB", on_progress_callback=on_progress)
+        yt = YouTube(url, proxies=proxy, on_progress_callback=on_progress)
         print(yt.title)
 
         ys = yt.streams.get_audio_only()
